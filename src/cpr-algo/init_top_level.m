e99df53172c0d50144eaf10bd513a3eae0050199
Eric Hillebrand <hillebre@mcmaster.ca>
2022-11-24 17:31:07 -0500

    Added Init file and data

---
+++ src/cpr-algo/init_top_level.m
@@ -0,0 +1,20 @@
+clc;
+clear all;
+close all;
+
+% load analogRead_A4.mat
+% load analogRead_A4_35sec.mat
+load analogRead_A4_LotsVariance.mat
+x = data.Time;
+y = data.Data;
+y = squeeze(y(1,1,:));
+plot(x,y)
+title("ADC Values")
+xlabel("Time (Sec)")
+
+%%
+% my_time = out.distance.Time;
+% distance = out.distance.data;
+% distance = squeeze(distance(1,1,:));
+% figure
+% plot(my_time, distance)
